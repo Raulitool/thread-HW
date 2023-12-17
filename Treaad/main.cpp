9bd63bb8ce6e093602b9105a4725a1c15a350dde
Dzhokhadze Raul <raulito2003@mail.ru>
2023-12-17 22:42:21 +0300

    new

--- Treaad/main.cpp
+++ Treaad/main.cpp
@@ -1,44 +1,59 @@
 #include <iostream>
-#include <vector>
+#include <fstream>
+#include <mutex>
+#include <shared_mutex>
+#include <string>
 #include <thread>
-#include <numeric>
 
-// Функция для подсчета суммы элементов в подмассиве
-int calculateSum(const std::vector<int>& array, int start, int end) {
-    int sum = 0;
-    for (int i = start; i < end; ++i) {
-        sum += array[i];
+class Logger {
+public:
+    Logger(const std::string& filename) : file(filename, std::ios_base::app) {}
+
+    void writeLog(const std::string& message) {
+        std::unique_lock<std::shared_mutex> lock(mutex);
+        file << message << "\n";
     }
-    return sum;
-}
+    std::string readLog() {
+        std::shared_lock<std::shared_mutex> lock(mutex);
+        std::string line;
+        if (file.is_open()) {
+            if (std::getline(file, line)) {
+                return line;
+            }
+        }
+        return ""; // Или какая-то другая обработка, если строка не прочитана
+    }
+
+    ~Logger() {
+        if (file.is_open()) {
+            file.close();
+        }
+    }
+
+private:
+    std::ofstream file;
+    mutable std::shared_mutex mutex;
+};
 
 int main() {
-    const int N = 20; // Размер массива
-    const int M = 4;  // Количество частей
+    Logger logger("log.txt");
 
-    std::vector<int> numbers = { 5, 3, 2, 4, 8, 9, 6, 1, 1, 7, 9, 3, 8, 2, 6, 3, 5, 5, 4, 7 };
+    // Пример записи и чтения из разных потоков
+    std::thread writer([&]() {
+        for (int i = 0; i < 5; ++i) {
+            logger.writeLog("Message " + std::to_string(i));
+        }
+        });
 
-    std::vector<std::thread> threads;
-    std::vector<int> partialSums(M, 0);
+    std::thread reader([&]() {
+        for (int i = 0; i < 5; ++i) {
+            std::string line = logger.readLog();
+            std::cout << "Read: " << line << std::endl;
+        }
+        });
 
-    // Создание потоков для подсчета суммы в каждой части массива
-    for (int i = 0; i < M; ++i) {
-        int start = i * (N / M);
-        int end = (i == M - 1) ? N : (i + 1) * (N / M);
-        threads.emplace_back([&, start, end, i]() {
-            partialSums[i] = calculateSum(numbers, start, end);
-            });
-    }
-
-    // Ожидание завершения выполнения всех потоков
-    for (auto& thread : threads) {
-        thread.join();
-    }
-
-    // Вычисление общей суммы
-    int totalSum = std::accumulate(partialSums.begin(), partialSums.end(), 0);
-
-    std::cout << "Total sum: " << totalSum << std::endl;
+    writer.join();
+    reader.join();
 
     return 0;
 }
